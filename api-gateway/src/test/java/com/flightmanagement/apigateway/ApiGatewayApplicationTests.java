4765ab35897b901ff9142d9cfc48301d540e4185
Manthina <laya-sree.manthina@capgemini.com>
2026-09-14 19:43:15 +0530

    Fix API Gateway test context

--- api-gateway/src/test/java/com/flightmanagement/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/flightmanagement/apigateway/ApiGatewayApplicationTests.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @ActiveProfiles("test")
-@TestPropertySource(properties = "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration,org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration")
 class ApiGatewayApplicationTests {
 
 	@Test
